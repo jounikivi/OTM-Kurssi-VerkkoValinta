446676fd3c9fb2088a5425136f7304b9b3a6416c
Jouni Kiviperä <jounikivipera@gmail.com>
2024-02-20 11:19:23 +0200

    muutoksia

--- VerkkoValinta/kauppa/views.py
+++ VerkkoValinta/kauppa/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from  .models import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def login_user(request):
+  return render(request, 'login.html')
+
+def logout_user(request):
+  pass
