498df311ff2bdec389d32c39af5e6f3d0ac10e5f
Jouni Kiviperä <jounikivipera@gmail.com>
2024-03-11 17:54:40 +0200

    Update models.py

--- VerkkoValinta/kauppa/models.py
+++ VerkkoValinta/kauppa/models.py
@@ -49,3 +49,15 @@ class Category(models.Model):
   
   class Meta:
     verbose_name_plural = "Categories"
+    
+class Product(models.Model):
+  name = models.CharField(max_length=200)
+  price = models.DecimalField(default=0, max_places=2, decimal_digits=6)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+  description = models.CharField(max_length=200, default='', blank=True)
+  image = models.ImageField(upload_to='uploads/product/')
+  is_sale = models.BooleanField(default=False)
+  sale_price = models.DecimalField(default=0, max_places=2, decimal_digits=6)
+  
+  def __str__(self):
+    return self.name
