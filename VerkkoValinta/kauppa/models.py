d08ddba7989d04e50d241006c7de1736603296d9
Jouni Kiviperä <jounikivipera@gmail.com>
2024-03-19 10:16:55 +0200

    Update models.py

--- VerkkoValinta/kauppa/models.py
+++ VerkkoValinta/kauppa/models.py
@@ -61,3 +61,15 @@ class Product(models.Model):
   
   def __str__(self):
     return self.name
+
+class Order(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+  quantity = models.IntegerField(default=1)
+  address = models.CharField(max_length=100, default='', blank=True)
+  phone = models.CharField(max_length=20, default='', blank=True)
+  date = models.DateField(default=datetime.datetime.today)
+  status = models.BooleanField(default=False)
+  
+  def __str__(self):
+    return self.product
