33f748793fb979ca6afa8d1dc80f01a91172cba8
Jouni Kiviperä <jounikivipera@gmail.com>
2024-03-19 10:26:49 +0200

    muutoksia

--- VerkkoValinta/kauppa/models.py
+++ VerkkoValinta/kauppa/models.py
@@ -52,12 +52,14 @@ class Category(models.Model):
     
 class Product(models.Model):
   name = models.CharField(max_length=200)
-  price = models.DecimalField(default=0, max_places=2, decimal_digits=6)
+  price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+  #price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
   category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
   description = models.CharField(max_length=200, default='', blank=True)
   image = models.ImageField(upload_to='uploads/product/')
   is_sale = models.BooleanField(default=False)
-  sale_price = models.DecimalField(default=0, max_places=2, decimal_digits=6)
+  sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+
   
   def __str__(self):
     return self.name
